2e813ae3f5f6ae8d116c53241259eac9dd4beafe
Hussein <tejanahmedhu@gmail.com>
2018-09-09 21:12:20 +0100

    merged footer into navbar

--- src/main/frontend/src/components/NavbarFooter.vue
+++ src/main/frontend/src/components/NavbarFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section id="footer">
+    <section id="footer" class="bg-info">
       <div class="container">
         <div class="row text-center text-xs-center text-sm-left text-md-left">
           <div class="col-xs-12 col-sm-4 col-md-4">
