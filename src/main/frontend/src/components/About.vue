5c75df4de4eac5704848b755e9413e0bf8ff92fd
Hussein <tejanahmedhu@gmail.com>
2018-09-09 16:02:49 +0100

    made changes to css

--- src/main/frontend/src/components/About.vue
+++ src/main/frontend/src/components/About.vue
@@ -18,14 +18,16 @@
         <div className="container">
           <div className="row">
             <div className="col-sm-12 col-lg-12">
-              <!--<h2>Our Values</h2>-->
-              <h4><strong>Introduction</strong> Lorem ipsum dolor sit amet, consecteur adipiscing elit.<br/> Mauris ultrices lorem sit amet  aliquam, sed tristique quam tincidunt.</h4>
-              <h4><strong>History</strong> Nullam lectus eros, dapibus quis ultrices amet, mattis in urna.<br/> Fusce rhoncus nibh non neque consequat placerat.</h4>
+              <h2 class="about-title">Introduction</h2>
+              <h4 class="about-content">QA Cinema is a champion in the premium entertainment cinema sector in the UK.
+                <br/> Committed to continued growth and development of its premium entertainment ‘big screen’ experience, QA Cinema delivers the greatest choice of content, combined with the finest technology in the best environment.</h4>
+              <h2 class="about-title">History</h2><br/>
+              <h4 class="about-content"> QA Cinema was founded in the UK following the acquisition of the Warner Village Cinemas in 2003 and is part of the largest cinema group in Europe.</h4>
             </div>
             <div className="col-sm-4">
-              <h2>Opening Hours:</h2>
-              <span className="glyphicon glyphicon-signal logo"></span>
-              <ul>
+              <h2 class="about-title">Opening Hours:</h2>
+
+              <ul class="about-content">
                 <li>Monday: 08.00 - 22:00</li>
                 <li>Tuesday: 08.00 - 22:00</li>
                 <li>Wednesday: 08.00 - 22:00</li>
@@ -50,5 +52,10 @@
 </script>
 
 <style scoped>
-
+.about-title{
+  float: left;
+}
+  .about-content{
+    float:left;
+  }
 </style>
